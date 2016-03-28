9af88a1c1dd76ec24efb2d92f7f589636bcd2e73
yutannihilation <yutani.ini@gmail.com>
2016-03-28 20:19:15 +0900

    improve document of Root

--- R/root.R
+++ R/root.R
@@ -1,6 +1,9 @@
 #' Root API
 #'
+#' Root is the top-level entity in the sync hierarchy.
+#'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/root}
+#' @name wndr_root
 #'
 #' @export
 wndr_get_root <- function() {
