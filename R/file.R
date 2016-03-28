288014a15a20c1a248898ce5cbfbac6d8573fa94
yutannihilation <yutani.ini@gmail.com>
2016-03-28 17:30:20 +0900

    add documentation of File

--- R/file.R
+++ R/file.R
@@ -1,18 +1,26 @@
 #' File API
+#'
+#' A task may contain files.
+#'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/file}
 #'
 #' @param id File ID
 #' @param task_id Task ID
 #' @param list_id List ID
+#' @details
+#' When you use \code{wndr_get_file}, you have to specify either one of \code{id}, \code{task_id}, \code{list_id}.
+#'
 #' @export
 wndr_get_file <- function(id = NULL, task_id = NULL, list_id = NULL) {
+  query <- create_scalar_list(id = NULL,
+                              task_id = task_id,
+                              list_id = list_id)
+
   if (!is.null(id)) {
     wndr_api(verb = "GET",
              path = "/api/v1/files",
              id   = id)
   } else {
-    query <- create_scalar_list(task_id = task_id,
-                                list_id = list_id)
     wndr_api(verb = "GET",
              path = "/api/v1/files",
              query = query)
