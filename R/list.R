435304d82d6f1c7a16bd7abcc6896c301ec3eeb4
yutannihilation <yutani.ini@gmail.com>
2016-03-28 18:08:46 +0900

    add document of List

--- R/list.R
+++ R/list.R
@@ -1,10 +1,37 @@
 #' List API
 #'
+#' All tasks created in Wunderlist belong to a list.
+#'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/list}
+#' @name wndr_list
 #'
 #' @param id List id
 #' @param title List title
-#' @param revision list revision
+#' @param revision List revision
+#' @param public Whether or not the list is public
+#'
+#' @examples
+#' \dontrun{
+#' # get all lists
+#' wndr_get_list()
+#'
+#' # get a list
+#' wndr_get_list(id = 1111)
+#'
+#' # create a list
+#' l <- wndr_create_list("test")
+#'
+#' # update the list
+#' wndr_update_list(id = l$id, revision = l$revision, title = "Test")
+#'
+#' # make the list public
+#' wndr_update_list(id = l$id, revision = l$revision, public = TRUE)
+#'
+#' # delete the list
+#' l <- wndr_get_list(id = l$id)
+#' wndr_delete_list(id = l$id, revision = l$revision + 2)
+#' }
+#'
 #' @export
 wndr_get_list <- function(id = NULL) {
   wndr_api(verb = "GET",
@@ -12,17 +39,17 @@ wndr_get_list <- function(id = NULL) {
            id   = id)
 }
 
+#' @rdname wndr_list
 #' @export
 wndr_create_list <- function(title) {
-  path <- "/api/v1/lists"
-
   wndr_api(verb = "POST",
-           path = path,
+           path = "/api/v1/lists",
            body = list(
              title = title
            ))
 }
 
+#' @rdname wndr_list
 #' @export
 wndr_update_list <- function(id, revision, title = NULL, public = NULL) {
   body <- create_scalar_list(title  = title, public = public)
@@ -34,6 +61,7 @@ wndr_update_list <- function(id, revision, title = NULL, public = NULL) {
            body = body)
 }
 
+#' @rdname wndr_list
 #' @export
 wndr_delete_list <- function(id, revision) {
   wndr_delete_common(id, revision, "/api/v1/lists")
