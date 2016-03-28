b492a0368b77d05674f2ef4d333f8d216bf55b36
yutannihilation <yutani.ini@gmail.com>
2016-03-28 19:30:08 +0900

    add document if Preview

--- R/preview.R
+++ R/preview.R
@@ -1,6 +1,13 @@
 #' File Previews
 #'
+#' A file preview is an image thumbnail for a file.
+#'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/file_preview}
+#' @name wndr_preview
+#'
+#' @param file_id File ID.
+#' @param platform Platform. Possible values are: mac, web, windows, iphone, ipad, android.
+#' @param size Size of screen. Possible values are: nonretina, retina
 #'
 #' @export
 wndr_get_preview <- function(file_id, platform = NULL, size = NULL) {
