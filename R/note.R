8e27768a80ed62e559c822bdfda398fa48bc4105
yutannihilation <yutani.ini@gmail.com>
2016-03-28 19:19:27 +0900

    add document of Note

--- R/note.R
+++ R/note.R
@@ -1,22 +1,43 @@
 #' Note API
 #'
+#' A note is a large text blobs belonging to tasks.
+#'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/note}
+#' @name wndr_note
+#'
+#' @param id Note ID
+#' @param task_id Task ID
+#' @param list_id List ID
+#' @param content Note content
+#' @param revision Revision
+#'
+#' @details
+#' When you use \code{wndr_get_note}, you have to specify either one of \code{id}, \code{task_id}, \code{list_id}.
+#'
+#' @examples
+#' \dontrun{
+#' # get notes
+#' l <- wndr_get_list()
+#' wndr_get_note(list_id = l$id[2])
+#'
+#' # create a note
+#' t <- wndr_get_task(list_id = l$id[2])
+#' n <- wndr_create_note(task_id = t$id[1], content = "test")
+#'
+#' # update the note
+#' wndr_update_note(id = n$id, revision = n$revision, content = "Test")
+#'
+#' # delete the note
+#' n <- wndr_get_note(id = n$id)
+#' wndr_delete_note(id = n$id, revision = n$revision)
+#' }
 #'
 #' @export
 wndr_get_note <- function(id = NULL, task_id = NULL, list_id = NULL) {
-  if (!is.null(id)) {
-    wndr_api(verb = "GET",
-             path = "/api/v1/notes",
-             id = id)
-  } else {
-    query <- create_scalar_list(task_id = task_id,
-                                list_id = list_id)
-    wndr_api(verb = "GET",
-             path = "/api/v1/notes",
-             query = query)
-  }
+  wndr_get_by_id_or_list_or_task(id, task_id, list_id, "/api/v1/notes")
 }
 
+#' @rdname wndr_note
 #' @export
 wndr_create_note <- function(task_id, content) {
   wndr_api(verb = "POST",
@@ -27,6 +48,7 @@ wndr_create_note <- function(task_id, content) {
            ))
 }
 
+#' @rdname wndr_note
 #' @export
 wndr_update_note <- function(id, revision, content) {
   wndr_api(verb = "PATCH",
@@ -38,6 +60,7 @@ wndr_update_note <- function(id, revision, content) {
            ))
 }
 
+#' @rdname wndr_note
 #' @export
 wndr_delete_note <- function(id, revision) {
   wndr_delete_common(id, revision, "/api/v1/notes")
