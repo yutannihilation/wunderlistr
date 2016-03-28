3d1f497b47dea5da179c173b4256597abbe6eefb
yutannihilation <yutani.ini@gmail.com>
2016-03-28 20:46:55 +0900

    improve document of User

--- R/user.R
+++ R/user.R
@@ -1,18 +1,23 @@
 #' User API
 #'
+#' All info related to the currently signed in user.
+#'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/user}
 #'
+#' @param list_id List ID
+#'
 #' @export
-wndr_get_user <- function() {
-  wndr_api(verb = "GET",
-           path = "/api/v1/user")
-}
+wndr_get_user <- function(list_id = NULL) {
+  if(is.null(list_id)) {
+    wndr_api(verb = "GET",
+             path = "/api/v1/user")
 
-#' @export
-wndr_get_users <- function(list_id = NULL) {
-  wndr_api(verb = "GET",
-           path = "/api/v1/users",
-           body = list(
-             list_id = list_id
-           ))
+  } else {
+    wndr_api(verb = "GET",
+             path = "/api/v1/users",
+             body = list(
+               list_id = list_id
+             ))
+
+  }
 }
