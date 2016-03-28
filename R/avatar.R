24cfe303645676d7a1fb974cfc617f46cf3ffc1e
yutannihilation <yutani.ini@gmail.com>
2016-03-28 17:14:22 +0900

    add document in avatar.R

--- R/avatar.R
+++ R/avatar.R
@@ -1,6 +1,10 @@
 #' Avatar API
 #'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/avatar}
+#'
+#' @param user_id User ID.
+#' @param size Image size. Possible values are 25, 28, 30, 32, 50, 54, 56, 60, 64, 108, 128, 135, 256, 270, 512.
+#' @param fallback If \code{FALSE}, no content will be returned when there is no custom avatar uploaded for the given \code{user_id}.
 #' @return matrix, the parsed result of response PNG image.
 #' @examples
 #' \dontrun{
@@ -8,7 +12,9 @@
 #'   plot(as.raster(x))
 #' }
 #' @export
-wndr_get_avatar <- function(user_id, size, fallback = FALSE) {
+wndr_get_avatar <- function(user_id, size = c(25, 28, 30, 32, 50, 54, 56, 60, 64, 108, 128, 135, 256, 270, 512), fallback = TRUE) {
+  size <- match.arg(size)
+
   wndr_api(verb = "GET",
            path = "/api/v1/avatar",
            query = list(
