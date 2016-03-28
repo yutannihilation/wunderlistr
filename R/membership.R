c0662ac7889f88417feae5c57b9fa61c80dee232
yutannihilation <yutani.ini@gmail.com>
2016-03-28 18:59:02 +0900

    add document of Membership

--- R/membership.R
+++ R/membership.R
@@ -1,6 +1,33 @@
 #' Membership API
 #'
+#' A Membership is the join model between Users and Lists.
+#'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/membership}
+#' @name wndr_membership
+#'
+#' @param id Membership ID
+#' @param list_id List ID
+#' @param user_id User ID
+#' @param email User email address
+#' @param muted Whether or not muted
+#' @param state Membership state. If set this to \code{"accepted"}, the membership is accepted
+#' @param revision Revision
+#'
+#' @examples
+#' \dontrun{
+#' # get memberships
+#' wndr_get_membership()
+#'
+#' # create a membership
+#' l <- wndr_get_list()
+#' m <- wndr_create_membership(list_id = l$id[2], user_id = 111111)
+#'
+#' # mark it as accepted
+#' wndr_update_membership(id = m$id, revision = m$revision, state = "accepted")
+#'
+#' # delete the membership
+#' wndr_delete_membership(id = m$id, revision = m$revision + 1)
+#' }
 #'
 #' @export
 wndr_get_membership <- function() {
@@ -8,9 +35,11 @@ wndr_get_membership <- function() {
            path = "/api/v1/memberships")
 }
 
+#' @rdname wndr_membership
 #' @export
 wndr_create_membership <- function(list_id, user_id = NULL, email = NULL, muted = NULL) {
   body <- create_scalar_list(user_id  = user_id, email = email)
+  body$list_id <- list_id
   body$muted <- muted
 
   wndr_api(verb = "POST",
@@ -18,6 +47,7 @@ wndr_create_membership <- function(list_id, user_id = NULL, email = NULL, muted 
            body = body)
 }
 
+#' @rdname wndr_membership
 #' @export
 wndr_update_membership <- function(id, revision, state = "accepted", muted = NULL) {
   wndr_api(verb = "PATCH",
@@ -31,6 +61,7 @@ wndr_update_membership <- function(id, revision, state = "accepted", muted = NUL
 }
 
 
+#' @rdname wndr_membership
 #' @export
 wndr_delete_membership <- function(id, revision) {
   wndr_delete_common(id, revision, "/api/v1/memberships")
