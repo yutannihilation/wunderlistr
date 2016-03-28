88df369dad658e7173c6722db78f82b3072363fd
yutannihilation <yutani.ini@gmail.com>
2016-03-28 18:01:44 +0900

    add document of Folder

--- R/folder.R
+++ R/folder.R
@@ -1,5 +1,33 @@
 #' Folder API
 #'
+#' A list may or may not belong to a folder.
+#'
+#' @name wndr_folder
+#'
+#' @param id Folder ID
+#' @param list_ids List IDs
+#' @param title Folder name
+#' @param revision Revision
+#'
+#' @examples
+#' \dontrun{
+#' # get all folders
+#' wndr_get_folder()
+#'
+#' # get a folder
+#' wndr_get_folder(id = 1111)
+#'
+#' # create a folder
+#' l <- wndr_get_list()
+#' f <- wndr_create_folder(title = "test", list_ids = l$id[1:3])
+#'
+#' # update the folder
+#' wndr_update_folder(id = f$id, revision = f$revision, list_ids = l$id[2:3])
+#'
+#' # delete the folder
+#' wndr_delete_folder(id = f$id, revision = f$revision)
+#' }
+#'
 #' @seealso \url{https://developer.wunderlist.com/documentation/endpoints/folder}
 #'
 #' @export
@@ -9,7 +37,7 @@ wndr_get_folder <- function(id = NULL) {
            id   = id)
 }
 
-
+#' @rdname wndr_folder
 #' @export
 wndr_create_folder <- function(title, list_ids) {
   wndr_api(verb = "POST",
@@ -20,6 +48,7 @@ wndr_create_folder <- function(title, list_ids) {
            ))
 }
 
+#' @rdname wndr_folder
 #' @export
 wndr_update_folder <- function(id, revision, title = NULL, list_ids = NULL) {
   wndr_api(verb = "PATCH",
@@ -31,6 +60,7 @@ wndr_update_folder <- function(id, revision, title = NULL, list_ids = NULL) {
            ))
 }
 
+#' @rdname wndr_folder
 #' @export
 wndr_delete_folder <- function(id, revision) {
   wndr_delete_common(id, revision, "/api/v1/folders")
